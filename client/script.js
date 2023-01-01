6f7b4a6e2fad9dc8166294b9ed553d6d0e9044b9
Rishi471616 <risidwivedi91@gmail.com>
2023-01-01 09:49:07 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
-    const response = await fetch('https://localhost:5000/', 
+    const response = await fetch('https://codey.onrender.com', 
     {
         method: 'POST',
         headers: {
